866753c068314d64db8d0d9404db440f5b2c9058
invoker1337 <91456039+invoker1337@users.noreply.github.com>
2021-10-07 11:01:11 +0200

    added alert() and document.write()

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+alert('Hello world');
+
+
+
+let favoriteFood="Apple, Banana & Avocado"
+document.write(favoriteFood)
