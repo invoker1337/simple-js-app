7734a32008065e419df2c02dd624134228622cfe
invoker1337 <91456039+invoker1337@users.noreply.github.com>
2021-10-07 11:08:59 +0200

    commented code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 alert('Hello world');
 
 
-
+// The let keyword is how you define a variable in JavaScript. Following that keyword is the name you want to give the variable
 let favoriteFood="Apple, Banana & Avocado"
 document.write(favoriteFood)
