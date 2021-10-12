abaa7e6f00d135c75aa1bf56d79f9d6b6bcb4ce5
invoker1337 <91456039+invoker1337@users.noreply.github.com>
2021-10-12 13:13:10 +0200

    changed  string name from "type" to "types"

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-  { name: 'Balbasaur', height: '0.7', type: ['grass', 'poison']},
-  { name: 'Graveler', height: '1', type: ['rock', 'ground']},
-  { name: 'Slowbro', height: '1.6', type: ['psychic', 'water' ]}
+  { name: 'Balbasaur', height: '0.7', types: ['grass', 'poison']},
+  { name: 'Graveler', height: '1', types: ['rock', 'ground']},
+  { name: 'Slowbro', height: '1.6', types: ['psychic', 'water' ]}
 ]
