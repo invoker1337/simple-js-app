c48b301c2b5fb84d9f310995b6c01d865316d7e9
invoker1337 <91456039+invoker1337@users.noreply.github.com>
2021-10-12 21:26:47 +0200

    included loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 let pokemonList = [
-  { name: 'Balbasaur', height: '0.7', types: ['grass', 'poison']},
-  { name: 'Graveler', height: '1', types: ['rock', 'ground']},
-  { name: 'Slowbro', height: '1.6', types: ['psychic', 'water' ]}
-]
+  { name: 'Balbasaur', height: ' 0.7 ', types: ['grass', 'poison']},
+  { name: 'Graveler', height: ' 1.0 ', types: ['rock', 'ground']},
+  { name: 'Slowbro', height: ' 1.6 ', types: ['psychic', 'water' ]}
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write("Name: " + pokemonList[i].name + " / height:" + pokemonList[i].height + " / Types: " + pokemonList[i].types);
+}
